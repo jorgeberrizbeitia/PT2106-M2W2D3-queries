a391e0c18b727e17fea0e396f3cfd45413c69296
Jorge <jorgeberrizbeitia@gmail.com>
2021-07-31 17:08:18 +0200

    lecture done

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // ** REQUIRES
 const express = require('express');
 const hbs = require('hbs');
+require("dotenv").config()
+const DogApi = require('doggo-api-wrapper');
+
+// ** API SETUP
+const myDog = new DogApi();
+// ! THIS IS WHERE WE WOULD USE .env FOR API CREDENTIALS
+console.log(process.env.SUPER_SECRET_API_KEY)
 
 // ** SERVER SETUP
 const app = express()
@@ -13,11 +20,44 @@ hbs.registerPartials(__dirname + '/views/partials');
 // ** MIDDLEWARES
 app.use(express.static(__dirname + '/public'));
 
+app.use(express.json()); // SETUP FOR BEING ABLE TO ACCESS THE BODY OF A POST REQUEST
+app.use(express.urlencoded({ extended: false })); // SETUP FOR BEING ABLE TO ACCESS THE BODY OF A POST REQUEST
+
 // ** ROUTES
 app.get('/', (req, res) => {
-  res.render('index')
+  myDog.getARandomDog()
+  .then( (data) => res.render('index', { data: data.message }))
+  .catch( (err) => console.log(err));
+})
+
+// ! GET ROUTE. PARAMS
+app.get('/dog/:name', (req, res) => {
+  console.log(req.params)
+  const { name } = req.params
+  // const name = req.params.name // same as above. But above is for GOOD developers
+  res.render("dog-profile", { name })
+})
+
+// ! GET ROUTE. QUERIES
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  const { breed } = req.query
+  myDog.getAllDogsByBreed(breed)
+  .then( (data) => res.render("search-results", { data: data.message }))
+  .catch( (err) => console.log(err));
+})
+
+// ! GET ROUTE. BODY
+app.post("/add-info", (req, res) => {
+  console.log(req.body)
+  // NOTE: GET REQUESTS DO NOT HAVE ACCESS TO THE REQ.BODY
 })
 
 // ** SERVER LISTENING
-const port = 3000
-app.listen(port, () => console.log(`🏃‍ on port ${port}`));
+console.log(process.env.PORT) // => the PORT coming from the .env file
+// .env files are used to store private information
+// currently in the .env file should be:
+// PORT=3000
+// SUPER_SECRET_API_KEY=BANANA
+// .env files should NEVER be published on github
+app.listen(process.env.PORT, () => console.log(`🏃‍ on port ${process.env.PORT}`));
